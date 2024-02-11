ca20f83b5fa52c04fc1133248943b04ce838e09f
monkii1 <102854745+monkii1@users.noreply.github.com>
2024-02-11 12:03:40 +0100

    add updated databasefile(no live server)

--- inc/db-connect.inc.php
+++ inc/db-connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=php_galerie', 'root', '', [
+    $pdo = new PDO('mysql:host=localhost;dbname=php_galerie', 'php_galerie', 'bADK22BSeG.2I!rz', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
